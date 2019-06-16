1519d23693e8e78086ece7cd85abfd60cb9f8de1
manojkumar1995 <manoj251995kumar@gmail.com>
2019-06-17 00:43:42 +0530

    app.js is _main file of application

---
+++ app.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const exphbs =require('express-handlebars');
+
+
+const app=express();
+
+//Load Routes
+
+const home= require('./routes/home');
+const classes = require('./routes/classes');
+
+//Handlebars MiddleWare
+app.engine('handlebars',exphbs({
+  defaultLayout: 'main'
+}));
+
+app.set('view engine','handlebars');
+
+//Use Routes
+app.use('/',home);
+app.use('/', classes);
+
+const port = 5000;
+
+app.listen(port,() => {
+  console.log(`Server Started on port ${port}`);
+});
