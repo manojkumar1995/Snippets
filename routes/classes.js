ed88b16b74c4c7cedce0ad6d85e8301cf2371354
manojkumar1995 <manoj251995kumar@gmail.com>
2019-06-18 18:41:22 +0530

    Added Typography Snippets

--- routes/classes.js
+++ routes/classes.js
@@ -1,21 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
 //Content Route
-router.get('/content', (req, res) => {
-  res.render('content');
+router.get("/content", (req, res) => {
+  res.render("content");
 });
 
 //Containers Route
-router.get('/containers', (req, res) => {
-  res.render('containers');
+router.get("/containers", (req, res) => {
+  res.render("containers");
 });
 
 //Buttons Routes
-router.get('/buttons', (req, res) => {
-  res.render('buttons');
+router.get("/buttons", (req, res) => {
+  res.render("buttons");
 });
 
+//Typography Routes
+router.get("/typography", (req, res) => {
+  res.render("typography");
+});
 
-module.exports = router;  
+module.exports = router;
