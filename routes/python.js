ea328990ac9b4ce51e1959c64ae5aae1058981c1
YangServer <manoj251995kumar@gmail.com>
2019-11-18 18:00:27 +0530

    Added Python landing page with images

--- routes/python.js
+++ routes/python.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/display", (req, res) => {
+  res.render("Python/python_display");
+});
+
 
 // ------------------------------Pythons-Language Routes Begin -------------------------//
 
