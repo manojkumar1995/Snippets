0ae87b98123ec02cc969b354569b2aaaeb05fb1d
manojkumar1995 <manoj251995kumar@gmail.com>
2019-11-21 23:49:28 +0530

    Added Express-Sessions | Flash Messages

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
+const flash = require('connect-flash');
+const session =require('express-session');
+const bodyParser =require('body-parser');
 const mongoose = require('mongoose');
+
 
 const app = express();
 
@@ -31,6 +35,27 @@ app.engine(
     defaultLayout: "main"
   })
 );
+
+//Body Parser middleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Express-Session Middleware
+app.use(session({
+  secret:'secret',
+  resave:false,
+  saveUninitialized:true
+}));
+
+app.use(flash());
+
+//Global Variables:
+app.use(function(req,res,next){
+  res.locals.sucess_msg=req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 app.set("view engine", "handlebars");
 
