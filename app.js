3d0ca214174c64e207928bb1f6bb155cc459730a
manojkumar1995 <manoj251995kumar@gmail.com>
2019-11-21 09:21:35 +0530

    Adding Authentication

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
+const mongoose = require('mongoose');
 
 const app = express();
 
+//Map global Promises  
+mongoose.Promise= global.Promise;
+
+//Connect to Mongodb
+mongoose.connect('mongodb://localhost/Snippets',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(()=> console.log('Mongodb connected ..'))
+.catch(err => console.log(err));
 
 //Load Routes
 const home = require("./routes/home");
 const bootstrap = require("./routes/bootstrap");
 const materialize = require("./routes/materialize");
 const python = require("./routes/python");
+
+const users = require("./routes/users");
 
 //Handlebars MiddleWare
 app.engine(
@@ -29,6 +42,8 @@ app.use("/", home);
 app.use("/bootstrap/classes", bootstrap)
 app.use("/materialize/classes", materialize)
 app.use("/python/language", python)
+app.use("/users/", users)
+
 //Page Not Found Route
 app.get('*', function (req, res) {
   res.render("404-page");
