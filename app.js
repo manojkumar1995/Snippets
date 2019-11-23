7c82313bf83ce646550964e4dcf8679890e4ed38
manojkumar1995 <manoj251995kumar@gmail.com>
2019-11-23 23:49:52 +0530

    Added locations Header

--- app.js
+++ app.js
@@ -77,6 +77,18 @@ app.set("view engine", "handlebars");
 //Static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  next();
+});  
+
 //Use Routes
 app.use("/", home);
 app.use("/bootstrap/classes", bootstrap)
